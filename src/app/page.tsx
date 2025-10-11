b66e23dc4d2fc60b1bf1bb7e5f8fe648492a7c06
10012 <nas7062@naver.com>
2025-10-11 22:07:03 +0900

    style : css 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,19 +7,19 @@ export default function Home() {
       <div className="flex-1 flex justify-center items-center">
         <Image src={logo} alt="로고" />
       </div>
-      <div className="flex-1 flex justify-center space-y-2 items-center flex-col">
-        <h1 className="text-4xl">지금 일어나고 있는 일</h1>
-        <h2 className="text-3xl">지금 가입하세요.</h2>
+      <div className="flex-1 flex justify-center space-y-4 items-center flex-col">
+        <h1 className="text-6xl">지금 일어나고 있는 일</h1>
+        <h2 className="text-4xl">지금 가입하세요.</h2>
         <Link
           href={"/flow/signup"}
-          className="px-4 py-2 border bg-sky-400 text-white rounded-lg"
+          className="px-4 py-2 border bg-sky-400 hover:bg-sky-500 text-white rounded-lg"
         >
           계정 만들기
         </Link>
         <h3 className="text-gray-400">이미 가입하셧나요?</h3>
         <Link
           href={"/flow/login"}
-          className="px-4 py-2 border bg-sky-400 text-white rounded-lg"
+          className="px-4 py-2 border bg-sky-400 hover:bg-sky-500 text-white rounded-lg"
         >
           로그인
         </Link>
