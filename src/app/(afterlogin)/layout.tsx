208e57c3c51257e302d0498273cf505d314c2323
10012 <nas7062@naver.com>
2025-10-11 22:00:39 +0900

    feat : 초기세팅 및 라우트 구성

--- src/app/(afterlogin)/layout.tsx
+++ src/app/(afterlogin)/layout.tsx
@@ -1,13 +1,11 @@
-import "./globals.css";
-
-export default function RootLayout({
+export default function AfterLoginLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <h2>루트 레이아웃</h2>
+      <h2>애프터 로그인 레이아웃</h2>
       <body>{children}</body>
     </html>
   );
