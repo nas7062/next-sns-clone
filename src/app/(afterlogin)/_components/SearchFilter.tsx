5923bd3c6bf31eade8d398498174276987b1a3fe
10012 <nas7062@naver.com>
2025-10-15 14:20:10 +0900

    feat : serachFilter에 shadcn 적용

--- src/app/(afterlogin)/_components/SearchFilter.tsx
+++ src/app/(afterlogin)/_components/SearchFilter.tsx
@@ -12,25 +12,27 @@ export default function SearchFilter() {
         <h2 className="text-2xl font-semibold">검색필터</h2>
         <div className="ml-4 space-y-1">
           <h3 className="text-lg font-semibold">사용자</h3>
-          <RadioGroup defaultValue="option-one">
-            <div className="flex items-center space-x-2">
-              <Label htmlFor="option-one">Option One</Label>
+          <RadioGroup defaultValue="option-one" className="w-[90%]">
+            <div className="flex items-center justify-between space-x-2">
+              <Label htmlFor="option-one">모든 사용자</Label>
               <RadioGroupItem value="option-one" id="option-one" />
             </div>
-            <div className="flex items-center space-x-2">
-              <Label htmlFor="option-two">Option Two</Label>
+            <div className="flex items-center space-x-2 justify-between">
+              <Label htmlFor="option-two">내가 팔로우하는 사람들</Label>
               <RadioGroupItem value="option-two" id="option-two" />
             </div>
           </RadioGroup>
           <h3 className="text-lg font-semibold">위치</h3>
-          <div className="flex justify-between w-[90%]">
-            <label htmlFor="">어디에서나</label>
-            <input type="radio" />
-          </div>
-          <div className="flex justify-between w-[90%]">
-            <label htmlFor="">현 위치 주변</label>
-            <input type="radio" />
-          </div>
+          <RadioGroup defaultValue="option-one" className="w-[90%]">
+            <div className="flex items-center justify-between space-x-2 ">
+              <Label htmlFor="option-one">어디에서나</Label>
+              <RadioGroupItem value="option-one" id="option-one" />
+            </div>
+            <div className="flex items-center space-x-2 justify-between">
+              <Label htmlFor="option-two">현 위치 주변</Label>
+              <RadioGroupItem value="option-two" id="option-two" />
+            </div>
+          </RadioGroup>
         </div>
       </div>
     );
