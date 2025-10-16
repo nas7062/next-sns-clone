a69304a1331fee45668ed07870139888a2080e32
10012 <nas7062@naver.com>
2025-10-16 21:49:03 +0900

    feat : commentForm 및 상세페이지 라우팅

--- src/app/(afterlogin)/[username]/status/[id]/page.tsx
+++ src/app/(afterlogin)/[username]/status/[id]/page.tsx
@@ -1,3 +1,22 @@
+import Post from "@/app/(afterlogin)/_components/Post";
+import { ArrowLeftCircle } from "lucide-react";
+import CommentForm from "../_components/CommentForm";
+
 export default function Page() {
-  return <div>상세페이지</div>;
+  return (
+    <div>
+      <div>
+        <ArrowLeftCircle className="w-8 h-8 relative top-2" />
+        <h3>게시하기</h3>
+      </div>
+      <Post />
+      <CommentForm />
+      <div>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
+    </div>
+  );
 }
