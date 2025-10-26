831625f237c292e9cda0b5fdf4acb18e5630b95e
10012 <nas7062@naver.com>
2025-10-26 20:03:48 +0900

    feat : trend 비로그인 생성 X

--- src/app/(afterlogin)/_components/TrendSection.tsx
+++ src/app/(afterlogin)/_components/TrendSection.tsx
@@ -5,21 +5,27 @@ import Trend from "./Trend";
 import { useQuery } from "@tanstack/react-query";
 import { getTrend } from "../_lib/getTrend";
 import { Hashtag } from "@/model/HashTag";
+import { useSession } from "next-auth/react";
 export default function TrendSection() {
   const pathname = usePathname();
+  const { data: session } = useSession();
   const { data } = useQuery<Hashtag[]>({
     queryKey: ["trends"],
     queryFn: getTrend,
     staleTime: 60000,
+    enabled: !!session?.user,
   });
   if (pathname === "/explore") return null;
 
-  return (
-    <div>
-      <h3 className="mt-6 text-xl ml-2 font-semibold">나를 위한 트렌드</h3>
-      {data?.map((trend) => (
-        <Trend trend={trend} key={trend.tagId} />
-      ))}
-    </div>
-  );
+  if (session?.user) {
+    return (
+      <div>
+        <h3 className="mt-6 text-xl ml-2 font-semibold">나를 위한 트렌드</h3>
+        {data?.map((trend) => (
+          <Trend trend={trend} key={trend.tagId} />
+        ))}
+      </div>
+    );
+  }
+  return <h3>트렌드를 가져올 수 없습니다.</h3>;
 }
---
+++ src/app/(afterlogin)/_lib/getFollowRecommends.tsx
@@ -0,0 +1,15 @@
+export async function getFollowRecommends() {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/followingPosts`,
+    {
+      next: {
+        tags: ["posts", "following"],
+      },
+    }
+  );
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
