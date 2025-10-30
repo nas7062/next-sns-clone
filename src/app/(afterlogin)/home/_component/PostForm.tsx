8c639de7e1825fc81c63bd4c944874f5d9832f18
10012 <nas7062@naver.com>
2025-10-30 10:32:57 +0900

    feat : 이미지 등록 및 미리보기 생성

--- src/app/(afterlogin)/home/_component/PostForm.tsx
+++ src/app/(afterlogin)/home/_component/PostForm.tsx
@@ -3,19 +3,69 @@
 import Image from "next/image";
 import logo from "@/../public/logo.svg";
 import { ImageDown } from "lucide-react";
-import { ChangeEvent, FormEventHandler, useRef, useState } from "react";
+import {
+  ChangeEvent,
+  ChangeEventHandler,
+  FormEventHandler,
+  useRef,
+  useState,
+} from "react";
 export default function PostForm() {
   const imageRef = useRef<HTMLInputElement>(null);
   const [content, setContent] = useState("");
+  const [preview, setPreview] = useState<
+    Array<{ dataUrl: string; file: File } | null>
+  >([]);
   const onChangeContent = (e: ChangeEvent<HTMLTextAreaElement>) => {
     setContent(e.target.value);
   };
 
   const onSubmit: FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
+    const formData = new FormData();
+    formData.append("content", content);
+    preview.forEach((p) => {
+      if (p) {
+        formData.append("images", p.file);
+      }
+    });
+
+    fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/posts`, {
+      method: "post",
+      credentials: "include",
+      body: formData,
+    });
   };
   const onClickImage = () => {
     if (imageRef.current) imageRef.current.click();
+  };
+
+  const onRemoveImage = (index: number) => () => {
+    setPreview((prevPreview) => {
+      const prev = [...prevPreview];
+      prev[index] = null;
+      return prev;
+    });
+  };
+
+  const onUpload: ChangeEventHandler<HTMLInputElement> = (e) => {
+    e.preventDefault();
+    if (e.target.files) {
+      Array.from(e.target.files).forEach((file, index) => {
+        const reader = new FileReader();
+        reader.onloadend = () => {
+          setPreview((prevPreview) => {
+            const prev = [...prevPreview];
+            prev[index] = {
+              dataUrl: reader.result as string,
+              file,
+            };
+            return prev;
+          });
+        };
+        reader.readAsDataURL(file);
+      });
+    }
   };
   return (
     <form action="" onSubmit={onSubmit}>
@@ -29,6 +79,12 @@ export default function PostForm() {
             value={content}
           />
         </div>
+        <div style={{ display: 'flex' }}>
+          {preview.map((v, index) => (
+            v && <div key={index} style={{ flex: 1 }} onClick={onRemoveImage(index)}>
+              <img src={v.dataUrl} alt="미리보기" style={{objectFit: "contain", width: '100%', maxHeight: 100}} />
+            </div>
+          ))}
         <div className="flex justify-between w-[90%] mx-auto">
           <div className="w-10 h-10 hover:bg-sky-200 rounded-full flex justify-center items-center">
             <input type="file" hidden ref={imageRef} />
